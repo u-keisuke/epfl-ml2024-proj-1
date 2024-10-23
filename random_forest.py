b5840398c8bf9922cd4ad932bbcf9658e8e5ded9
Dim Gor <dimgor2003@gmail.com>
2024-10-23 20:42:20 +0200

    xgboost selfimplement

--- random_forest.py
+++ random_forest.py
@@ -6,6 +6,7 @@ import time
 
 from utils_copy.decision_tree import entropy, gini, variance, mad_median, DecisionTree
 from utils_copy.decision_tree import RandomForest
+from utils_copy.boost_tree import BoostForest
 
 
 x_train = pd.read_csv('dataset/x_train.csv', index_col=0)
@@ -15,21 +16,21 @@ y_train[y_train==-1] = 0
 x_train = np.array(x_train)
 y_train = np.array(y_train)
 print(x_train.shape)
-x_train = x_train[:10, :]
-y_train = y_train[:10, :]
+x_train = x_train[:1000, :]
+y_train = y_train[:1000, :]
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
 
 
-classifier_forest = RandomForest(max_depth=10, criterion_name='entropy', 
-                                 random_state=None, num_trees=30)
+classifier_forest = BoostForest(max_depth=5, learning_rate=0.2, random_state=None, num_trees=100)
 print("starting" )
 start = time.time()
 classifier_forest.fit(x_train, y_train)
 print("finished", time.time()-start)
 
 y_val_pred = classifier_forest.predict(x_val)
-accuracy_forest = accuracy_score(y_val, y_val_pred)
+print(y_val_pred)
+accuracy_forest = accuracy_score(y_val, y_val_pred.round())
 print(accuracy_forest)
 
-f1_scoree = f1_score(y_val, y_val_pred)
+f1_scoree = f1_score(y_val, y_val_pred.round())
 print(f1_scoree)
--- utils_copy/boost_tree.py
+++ utils_copy/boost_tree.py
@@ -3,21 +3,6 @@ from sklearn.base import BaseEstimator
 from sklearn.preprocessing import OneHotEncoder
 
 
-def entropy(y):  
-    EPS = 0.0005  
-    return - np.sum(y * np.log(y + EPS))
-    
-def gini(y):
-    return 1 - np.sum(y * y)
-    
-def variance(y):
-    mean = np.mean(y)
-    return np.mean((y - mean) * (y - mean))
-
-def mad_median(y):
-    return np.mean(np.abs(y - np.median(y)))
-
-
 def one_hot_encode(n_classes, y):
     y_one_hot = np.zeros((len(y), n_classes), dtype=float)
     y_one_hot[np.arange(len(y)), y.astype(int)[:, 0]] = 1.
@@ -26,130 +11,122 @@ def one_hot_encode(n_classes, y):
 def one_hot_decode(y_one_hot):
     return y_one_hot.argmax(axis=1)[:, None]
 
+def prob2logodds(p):
+    return np.log(p/(1-p))
+
+def logodss2prob(log_odds):
+    return np.exp(log_odds)/(1+np.exp(log_odds))
 
 class Node:
-    def __init__(self, feature_index, threshold, proba=0):
+    def __init__(self, feature_index, threshold, logodds=None):
         self.feature_index = feature_index
         self.value = threshold
-        self.proba = proba
+        self.logodds = logodds
         self.left_child = None
-        self.right_child = None
+        self.right_child = None 
+       
         
-        
-class DecisionTree():
-    all_criterions = {
-        'gini': (gini, True), # (criterion, classification flag)
-        'entropy': (entropy, True),
-        'variance': (variance, False),
-        'mad_median': (mad_median, False)
-    }
-
+class BoostTree():
+    """
+    change the VALUES in the leaves: in case of 
+    """
     def __init__(self, n_classes=None, max_depth=np.inf, min_samples_split=2, 
-                 criterion_name='gini', debug=False, max_features=None, 
-                 random_state=None, replace=False):
-
-        assert criterion_name in self.all_criterions.keys(), 'Criterion name must be on of the following: {}'.format(self.all_criterions.keys())
+                 max_features=None, 
+                 random_state=None, replace=False, 
+                 gamma_regularizer=0, lambda_regularizer=0):
         
         self.n_classes = n_classes
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
-        self.criterion_name = criterion_name
         self.depth = 0
         self.root = None # Use the Node class to initialize it later
-        self.debug = debug
         self.max_features = max_features
         self.random_state = random_state
         self.replace = replace
+        self.gamma_regularizer = gamma_regularizer
+        self.lambda_regularizer = lambda_regularizer
 
-        
-    def make_split(self, feature_index, threshold, X_subset, y_subset):
+    def calculate_similarity(self, residuals):
+        similarity = residuals.sum()**2 
+        similarity /= len(residuals) + self.lambda_regularizer
+        return similarity
 
+    def make_split(self, feature_index, threshold, X_subset, y_subset, residuals):
         index_left = X_subset[:, feature_index] < threshold
         index_right = X_subset[:, feature_index] >= threshold
-        X_left, y_left = X_subset[index_left, :], y_subset[index_left]
-        X_right, y_right = X_subset[index_right, :], y_subset[index_right]
-        
-        return (X_left, y_left), (X_right, y_right)
+        X_left, y_left, residualsleft = X_subset[index_left, :], y_subset[index_left], residuals[index_left]
+        X_right, y_right, residuals_right = X_subset[index_right, :], y_subset[index_right], residuals[index_right]
+        return (X_left, y_left, residualsleft), (X_right, y_right, residuals_right)
     
 
-    def choose_best_split(self, X_subset, y_subset):
-        H = self.criterion
-        
-        #Bruteforce selection. Can we do better?
+    def choose_best_split(self, X_subset, y_subset, residuals):
+        # Bruteforce selection. Can we do better?
         if self.random_state:
             np.random.seed(self.random_state)
         feat_idxs = np.random.choice(len(X_subset[0]), self.max_features, replace=self.replace)
         X_subset = X_subset[:, feat_idxs]
 
-        best_loss = np.inf
+        best_gain = -np.inf
         feature_index = 0
         threshold = 0
+        
+        similarity = self.calculate_similarity(residuals)
         for i in range(len(X_subset[0])):
             X_featured = X_subset[:, i]
             thresholds = np.unique(X_featured)
             for thread in thresholds:
-                L = y_subset[X_featured < thread]
-                R = y_subset[X_featured >= thread]               
+                L = residuals[X_featured < thread]
+                R = residuals[X_featured >= thread] 
                 if not (R.any() and L.any()):
                     continue
-                if self.classification:
-                    hist_R = np.sum(R, axis=0)
-                    hist_L = np.sum(L, axis=0)
-                    loss = len(R) / len(y_subset) * H( hist_R / len(R)) +  len(L) / len(y_subset) * H(hist_L / len(L))
-                else:
-                    loss = len(R) / len(y_subset) * H(R) + len(L) / len(y_subset) * H(L) 
-                if loss < best_loss:
+                gain = self.calculate_similarity(L) +  self.calculate_similarity(R) - similarity
+
+                if gain > best_gain and gain>=self.gamma_regularizer:
                     threshold = thread
                     feature_index = i
-                    best_loss = loss
+                    best_gain = gain
+        if best_gain<0:
+            return None, None #to unpack            
         return feat_idxs[feature_index], threshold
     
-    def make_tree(self, X_subset, y_subset, depth):
+    def make_tree(self, X_subset, y_subset, depth, residuals):
         n_labels = len(np.unique(y_subset)) 
-
         if (depth == 1 or n_labels == 1):
-            if self.classification:
-                value = np.sum(y_subset, axis=0)
-                proba = value / len(y_subset)
-                return Node(feature_index=None, threshold=value.argmax(), proba=proba)
-            elif self.criterion_name == "variance":
-                return Node(feature_index=None, threshold=np.mean(y_subset))
-            elif self.criterion_name == "mad_median":
-                return Node(feature_index=None, threshold=np.median(y_subset))  
+            value = np.sum(y_subset, axis=0)
+            similarity = self.calculate_similarity(residuals)
+            return Node(feature_index=None, threshold=value.argmax(), logodds=similarity)
             
-
-        best_feat, best_thresh = self.choose_best_split(X_subset, y_subset)
+        best_feat, best_thresh = self.choose_best_split(X_subset, y_subset, residuals)
+        if not best_feat: # gain is negative: stop expanding the tree
+            value = np.sum(y_subset, axis=0)
+            similarity = self.calculate_similarity(residuals)
+            return Node(feature_index=None, threshold=value.argmax(), logodds=similarity)
+        
         new_node = Node(best_feat, best_thresh)
-        (X_left, y_left), (X_right, y_right) = self.make_split(best_feat, best_thresh, X_subset, y_subset)
+        (X_left, y_left, residuals_Left), (X_right, y_right, residuals_Right) = \
+            self.make_split(best_feat, best_thresh, X_subset, y_subset, residuals)
 
         if len(X_left) < self.min_samples_split or len(X_right) < self.min_samples_split:
-            if self.classification:
-                value = np.sum(y_subset, axis=0)
-                proba = value / len(y_subset)
-                return Node(feature_index=None, threshold=value.argmax(), proba=proba)
-            elif self.criterion_name == "variance":
-                return Node(feature_index=None, threshold=np.mean(y_subset))
-            elif self.criterion_name == "mad_median":
-                return Node(feature_index=None, threshold=np.median(y_subset))
-            
-        new_node.left_child = self.make_tree(X_left, y_left, depth-1)
-        new_node.right_child = self.make_tree(X_right, y_right, depth-1)
+            value = np.sum(y_subset, axis=0)
+            similarity = self.calculate_similarity(residuals)
+            return Node(feature_index=None, threshold=value.argmax(), logodds=similarity)
+
+        new_node.left_child = self.make_tree(X_left, y_left, depth-1, residuals_Left)
+        new_node.right_child = self.make_tree(X_right, y_right, depth-1, residuals_Right)
 
         return new_node
-            
 
-    def fit(self, X, y):
-        
+
+    def fit(self, X, y, previous_predictions):
         assert len(y.shape) == 2 and len(y) == len(X), 'Wrong y shape'
-        self.criterion, self.classification = self.all_criterions[self.criterion_name]
-        if self.classification:
-            if self.n_classes is None:
-                self.n_classes = len(np.unique(y))
-            y = one_hot_encode(self.n_classes, y)
+
+        if self.n_classes is None:
+            self.n_classes = len(np.unique(y))
+        y = one_hot_encode(self.n_classes, y)
         if not self.max_features:
             self.max_features = len(X[0])
-
-        self.root = self.make_tree(X, y, self.max_depth)
+        residuals = np.sum(y, axis=1) - previous_predictions
+        self.root = self.make_tree(X, y, self.max_depth, residuals)
     
     def predict(self, X):
         pred = np.zeros(len(X))
@@ -157,102 +134,85 @@ class DecisionTree():
             pred[i] = self.pass_tree(X[i, :], self.root)
         return pred
         
-    def predict_proba(self, X):
-        assert self.classification, 'Available only for classification problem'
-        pred_prob = np.zeros((len(X), self.n_classes))
+    def predict_logodds(self, X):
+        pred_logodds = np.zeros((len(X)))
         for i in range(len(X)): 
-            pred_prob[i] = self.pass_tree(X[i, :], self.root, prob_include=True)
-        return pred_prob
+            pred_logodds[i] = self.pass_tree(X[i, :], self.root, logodds_include=True)
+        return pred_logodds
     
-    def pass_tree(self, X, node, prob_include=False):
+    def pass_tree(self, X, node, logodds_include=False):
         if node.feature_index is None:
-            if not prob_include:
+            if not logodds_include:
                 return node.value
             else:
-                return node.proba
+                return node.logodds
 
         if X[node.feature_index] < node.value:
-            return self.pass_tree(X, node.left_child, prob_include=prob_include)
+            return self.pass_tree(X, node.left_child, logodds_include=logodds_include)
         else:
-            return self.pass_tree(X, node.right_child, prob_include=prob_include)
-
+            return self.pass_tree(X, node.right_child, logodds_include=logodds_include)
+    
 
 class BoostForest():
-    clasification_loss = {
-        'gini': True, # (criterion, classification flag)
-        'entropy': True,
-        'variance': False,
-        'mad_median': False
-    }
+    """
+    add regularizer???
+    """
+
     def __init__(self, n_classes=None, max_depth=np.inf, min_samples_split=2, 
-                 criterion_name='gini', debug=False, reduce_features=True,
-                 random_state=None, num_trees=10, classif_type="common" ):
+                 reduce_features=True,
+                 random_state=None, classif_type="common",
+                 depth=3, num_trees=10,
+                 learning_rate=0.3,
+                 lambda_regularizer=0, gamma_regularizer=0):
         
         self.n_classes = n_classes
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
-        self.criterion_name = criterion_name
-        self.depth = 0
+        self.depth = depth
         self.root = None # Use the Node class to initialize it later
-        self.debug = debug
         self.reduce_features = reduce_features
         self.random_state = random_state
         self.num_trees = num_trees
         assert classif_type in {"common", "proba", "both"}
         self.classif_type = classif_type
+        self.lambda_regularizer = lambda_regularizer # regularize the scores
+        self.gamma_regularizer = gamma_regularizer # regularize the number of leaves
+        self.learning_rate = learning_rate
 
-    def fit(self, X,y):
+    def fit(self, X, y):
         Forest = []
-        self.classification = self.clasification_loss[self.criterion_name]
-        if self.classification and self.n_classes is None:
-                self.n_classes = len(np.unique(y))
+        self.n_classes = len(np.unique(y))
 
         if self.reduce_features:
             max_features = np.around(np.sqrt(len(X[0]))).astype(int)
         
+        previous_prediction = np.random.rand(len(X)) # any better initializations?
+        logodds = prob2logodds(previous_prediction)
+        
         for _ in range(self.num_trees):
-            class_estimator = DecisionTree(n_classes=self.n_classes, max_depth=self.max_depth, 
+            class_estimator = BoostTree(n_classes=self.n_classes, max_depth=self.max_depth, 
                                             min_samples_split=self.min_samples_split, 
-                                            criterion_name=self.criterion_name, 
                                             max_features=max_features, 
-                                            replace=False) # random_state=None, 
+                                            replace=False) 
 
-            objects_idxs = np.random.choice(len(X[:, 0]), len(X[:, 0]), replace=True)
-            X_subset = X[objects_idxs, :]
-            y_subset = y[objects_idxs]
+            # objects_idxs = np.random.choice(len(X[:, 0]), len(X[:, 0]), replace=True)
+            # X_subset = X[objects_idxs, :]
+            # y_subset = y[objects_idxs]
+            # choose random features:
+            feature_ids = np.random.choice(len(X[0, :]), len(X[0, :]), replace=True)
+            X_subset = X[:, feature_ids]
+            
+            print(X_subset.shape)
 
-            class_estimator.fit(X_subset, y_subset)
-            Forest.append(class_estimator)
+            class_estimator.fit(X_subset, y, previous_prediction)
+            logodds += self.learning_rate * class_estimator.predict_logodds(X_subset)
+            previous_prediction = logodss2prob(logodds) # for training the next tree
+            Forest.append((class_estimator, self.learning_rate))
         self.Forest = Forest
 
     def predict(self, X):
-        if self.classification:
-            prediction = np.zeros((len(X), self.n_classes))
-            # #by probabilities
-            if self.classif_type == "proba":
-                for Tree in self.Forest:
-                    prediction = + Tree.predict_proba(X)
-            # by most common label
-            elif self.classif_type == "common":
-                for Tree in self.Forest:
-                    labels = Tree.predict(X).astype(int)
-                    prediction[np.arange(len(X[:,0])), labels] += 1
-            elif self.classif_type == "both":
-                for Tree in self.Forest:
-                    prediction = + Tree.predict_proba(X)
-
-                prediction1 = np.zeros((len(X), self.n_classes))
-                for Tree in self.Forest:
-                    labels = Tree.predict(X).astype(int)
-                    prediction1[np.arange(len(X[:,0])), labels] += 1
-                pred1 = pred = np.argmax(prediction1, axis=1)
-            pred = np.argmax(prediction, axis=1)
-        else:    
-            pred = np.zeros(len(X))
-            for Tree in self.Forest:
-                pred += Tree.predict(X)
-            pred = pred / self.num_trees   
-        if self.classif_type == "both":
-            return pred, pred1
-        return pred
+        logodds_include = np.zeros((len(X), self.n_classes))
+        for Tree, lr in self.Forest:
+            logodds_include = + lr * Tree.predict_logodds(X)
+        return logodss2prob(logodds_include)
          
