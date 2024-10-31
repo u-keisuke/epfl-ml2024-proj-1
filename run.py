e251c786899f071c30fd5689304f7ecbb4328abf
Dim Gor <dimgor2003@gmail.com>
2024-10-31 16:40:23 +0100

    deleted all redundant; renamed files; rewrote external libraries

--- run.py
+++ run.py
@@ -1,39 +1,60 @@
-import argparse
+import time
 
-import numpy as np
+import numpy as np 
 
-from utils.helpers import create_csv_submission, load_csv_data
-from utils.random import set_random_seed
+from boost_tree import BoostForest
+from utils.helpers import create_csv_submission, load_train_data, train_test_split, load_test_data, accuracy, f1_score
 
 
-def main(data_path, output_path, model_name):
-    set_random_seed()
+####################################################### Training part
+def train_model():
+    x_train, y_train = load_train_data('dataset')
 
-    # 1. Load the data
-    data_path = "dataset"
-    x_train, x_test, y_train, train_ids, test_ids = load_csv_data(data_path)
+    x_train = np.nan_to_num(x_train)
+    
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    y_train[y_train==-1] = 0
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1, random_state=42)
+    x_train = x_train[:100, :]
+    y_train = y_train[:100]
+    assert len(y_train.shape)==1, "y should be one dimensional array"
 
-    # 2. Predict
-    if model_name == "monkey":
-        from models.monkey import make_prediction
-        y_pred = make_prediction(model_name, x_train, x_test, y_train, train_ids, test_ids)
+    classifier_forest = BoostForest(max_depth=6, lr=0.1, decay_rate=0.95, decay_interval=None, #40
+                                    random_state=42, num_trees=16, cover=1, max_features=0.8,
+                                    lambda_regularizer=1, gamma_regularizer=0) # regularization to choose
+    start = time.time()
+    sample_weights = np.where(y_train == 1, 3.5, 1) # make y=1 more important for loss function
+    file_path = ".../model.pkl"
+    print("loaded the previous model's", len(classifier_forest.Forest), "trees")
+    classifier_forest.fit(x_train, y_train, sample_weights, file_path)
+    classifier_forest.save_model(file_path)
+    print("It took us", time.time()-start, "seconds")
 
-    elif model_name == "lightgbm":
-        from models.light_gbm import make_prediction
-        y_pred = make_prediction(model_name, x_train, x_test, y_train, train_ids, test_ids)
-        
-    else:
-        raise ValueError(f"Model {model_name} not found")
+    y_val_pred = classifier_forest.predict(x_val)
+    accuracy_forest = accuracy(y_val, y_val_pred.round())
+    val_f1 = f1_score(y_val, y_val_pred.round())
+    print(f'Validation Accuracy: {accuracy_forest * 100:.2f}%')
+    print(f'Validation F1 Score: {val_f1:.4f}')
 
-    # 3. Create the submission file
-    create_csv_submission(test_ids, y_pred, output_path)
+
+####################################################### Test part
+def test_model(file_path="model7.pkl"):
+
+    classifier_forest = BoostForest() 
+    classifier_forest.load_model(file_path, num_trees=None)
+
+    x_test, test_ids= load_test_data('dataset')
+    x_test = np.nan_to_num(x_test)
+    x_test = np.array(x_test)
+    y_pred = classifier_forest.predict(x_test)
+    # pos = sum(y_pred.round())
+    # neg = len(y_pred) - pos
+    # print(pos, neg)
+    y_pred = 2 * (y_pred.round()) - 1
+    create_csv_submission(test_ids, y_pred, "prediction")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate predictions")
-    parser.add_argument("--data_path", type=str, default="dataset", help="Path to the data folder")
-    parser.add_argument("--output_path", type=str, default="submission.csv", help="Path to the output file")
-    parser.add_argument("--model", type=str, default="monkey", help="Model to use for prediction")
-    args = parser.parse_args()
-
-    main(args.data_path, args.output_path, args.model)
+    test_model(file_path="model7.pkl")
+    # train_model(file_path="...")
--- utils/helpers.py
+++ utils/helpers.py
@@ -4,21 +4,35 @@ import os
 import numpy as np
 
 
-def load_csv_data(data_path, sub_sample=False):
+def accuracy(y_pred, y_true):
+    return np.mean(y_pred == y_true)
+   
+def f1_score(y_pred, y_true):
+    ind = (y_true == 1)
+    tp = sum(y_true[ind] == y_pred[ind])
+    fn = sum(y_true[ind] != y_pred[ind])
+
+    ind = (y_true == -1)
+    fp = sum(y_true[ind] != y_pred[ind])
+
+    pr = tp / (tp + fp) if (tp + fp) > 0 else 0
+    rec = tp / (tp + fn) if (tp + fn) > 0 else 0
+    f1 = 2 * pr * rec / (pr + rec) if (pr + rec) > 0 else 0
+
+    return f1
+
+def load_train_data(data_path, sub_sample=False):
     """
-    This function loads the data and returns the respectinve numpy arrays.
-    Remember to put the 3 files in the same folder and to not change the names of the files.
+    This function loads the training data and returns the respective numpy arrays.
 
     Args:
-        data_path (str): datafolder path
-        sub_sample (bool, optional): If True the data will be subsempled. Default to False.
+        data_path (str): data folder path
+        sub_sample (bool, optional): If True, the data will be subsampled. Default is False.
 
     Returns:
         x_train (np.array): training data
-        x_test (np.array): test data
         y_train (np.array): labels for training data in format (-1,1)
         train_ids (np.array): ids of training data
-        test_ids (np.array): ids of test data
     """
     y_train = np.genfromtxt(
         os.path.join(data_path, "y_train.csv"),
@@ -30,23 +44,36 @@ def load_csv_data(data_path, sub_sample=False):
     x_train = np.genfromtxt(
         os.path.join(data_path, "x_train.csv"), delimiter=",", skip_header=1
     )
-    x_test = np.genfromtxt(
-        os.path.join(data_path, "x_test.csv"), delimiter=",", skip_header=1
-    )
 
-    train_ids = x_train[:, 0].astype(dtype=int)
-    test_ids = x_test[:, 0].astype(dtype=int)
     x_train = x_train[:, 1:]
-    x_test = x_test[:, 1:]
 
-    # sub-sample
+    # Sub-sample
     if sub_sample:
         y_train = y_train[::50]
         x_train = x_train[::50]
         train_ids = train_ids[::50]
 
-    return x_train, x_test, y_train, train_ids, test_ids
+    return x_train, y_train
 
+def load_test_data(data_path):
+    """
+    This function loads the test data and returns the respective numpy arrays.
+
+    Args:
+        data_path (str): data folder path
+
+    Returns:
+        x_test (np.array): test data
+        test_ids (np.array): ids of test data
+    """
+    x_test = np.genfromtxt(
+        os.path.join(data_path, "x_test.csv"), delimiter=",", skip_header=1
+    )
+
+    test_ids = x_test[:, 0].astype(dtype=int)
+    x_test = x_test[:, 1:]
+
+    return x_test, test_ids
 
 def create_csv_submission(ids, y_pred, name):
     """
@@ -69,3 +96,20 @@ def create_csv_submission(ids, y_pred, name):
         writer.writeheader()
         for r1, r2 in zip(ids, y_pred):
             writer.writerow({"Id": int(r1), "Prediction": int(r2)})
+
+def train_test_split(X, y, test_size=0.1, random_state=None):
+    if random_state is not None:
+        np.random.seed(random_state)
+    
+    n_samples = len(X)
+    n_val = int(n_samples * test_size)
+    
+    indices = np.arange(n_samples)
+    np.random.shuffle(indices)
+    
+    val_indices = indices[:n_val]
+    train_indices = indices[n_val:]
+    X_train, X_val = X[train_indices], X[val_indices]
+    y_train, y_val = y[train_indices], y[val_indices]
+    
+    return X_train, X_val, y_train, y_val
