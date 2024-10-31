dd3c062064b316b3c18644b10d934389815ebff0
Dim Gor <dimgor2003@gmail.com>
2024-10-31 16:08:33 +0100

    removed redundant files

--- utils_copy/__init__.py
+++
@@ -1,9 +0,0 @@
-"here are some basic things from ML like knn, pca"
-from .decision_tree import DecisionTree, RandomForest
-from .pca import PCA
-
-__all__ = [
-    "DecisionTree", "RandomForest",
-    "PCA"
-
-]
